dca6a1609525e9e724ed5ed0691a9a30e3b851ba
rxliuli <rxliuli@gmail.com>
2025-08-22 10:07:57 +0800

    feat: update deploy config support spa

--- worker/index.ts
+++ worker/index.ts
@@ -1,5 +1,20 @@
 export default {
   async fetch(request: Request, env: any) {
-    return env.ASSETS.fetch(request)
+    const url = new URL(request.url)
+    
+    const staticExtensions = ['.js', '.css', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico', '.woff', '.woff2', '.ttf']
+    const hasExtension = staticExtensions.some(ext => url.pathname.endsWith(ext))
+    
+    if (hasExtension) {
+      return env.ASSETS.fetch(request)
+    }
+    
+    const indexRequest = new Request(new URL('/index.html', url.origin), {
+      method: request.method,
+      headers: request.headers,
+      body: request.body
+    })
+    
+    return env.ASSETS.fetch(indexRequest)
   },
 }
